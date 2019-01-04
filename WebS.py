c6299d68adfde68b2316f7d540adab0f94fe00de
hitgo00 <41156157+hitgo00@users.noreply.github.com>
2019-01-04 17:30:42 +0530

    Create WebS.py

---
+++ WebS.py
@@ -0,0 +1,26 @@
+import requests
+import bs4
+
+print("\nWelcome to News4u..!!\n")
+print("Topics:\n\n all\n national    business\n sports      world\n politics    technology\n startup     entertainment\n science     automobile\n")
+topic = input("Enter a topic from above to get news:  \n")
+
+if topic=="all":
+    res = requests.get('https://inshorts.com/en/read/')
+else:
+    res = requests.get('https://inshorts.com/en/read/' + topic)
+
+
+soup = bs4.BeautifulSoup(res.text, 'lxml')
+
+head = soup.find_all(itemprop="headline")
+
+url = soup.select(".source")
+print("\nTop "+topic+" news are:\n")
+
+for i in range(10):
+    print(head[i].getText())
+    print(url[i].get("href"))
+    print("\n")
+
+
