a53c83d92f52aa05349124bcd7840175da2dd2bc
Hitesh <hitgo17@gmail.com>
2019-07-26 01:21:29 +0530

    added comments and structed main file
    Signed-off-by: Hitesh <hitgo17@gmail.com>

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
 from flask import Flask ,render_template , url_for ,request
-# from flask_sqlalchemy import SQLAlchemy
 import requests
 import bs4
 import re
 
 app=Flask('__name__')
-# app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///test.db'
-# db=SQLAlchemy(app)
 
-# class Todo(db.Mode1):
-#     id=db.col
 
 @app.route('/',methods=['POST','GET'])
 def index():
@@ -22,26 +17,30 @@ def index():
         else:
             res = requests.get('https://inshorts.com/en/read/' + topic)
 
+# parsing the document received from requests using beautiful soup
         soup = bs4.BeautifulSoup(res.text, 'lxml')
 
+# getting /extracting the heading of news from parsed beautiful soup object
         head = soup.find_all(itemprop="headline")
 
+
+# getting /extracting the URL of source of news from parsed beautiful soup object
         url = soup.select(".source")
 
+
+# getting /extracting the image related to news from parsed beautiful soup object
         image=soup.select(".news-card-image")
 
+# ^ above  actions returns an array of objects
 
 
-
+# adding scraped data into an array so that it can easily be rendered
         for i in range(16):
-            print(re.search('(?P<url>https?://[^\s]+)',image[i].get("style") ).group("url"))
-
             item=dict(title=head[i].getText(),source =url[i].get("href"),image_url=re.search('(?P<url>https?://[^\s]+)',image[i].get("style") ).group("url"))
             news.append(item)
-            # news_title.append(head[i].getText())
-            # news_url.append(url[i].get("href"))
-            # print("\n")
-        # return (news_title[2])
+
+
+# added exception  handling and rendering using jinja2 templates
         try:
             return render_template('news4u.html',news_topic=topic,all_news=news)
         except:
